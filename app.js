b3e50e2118c693acf3ab819c40b8f5d87abdbb6a
abhisheksankhla121 <abhisheksankhla121@gmail.com>
2024-06-15 18:05:38 +0530

    add frontend url commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { config } from "dotenv"
 import inventoryRoute from "./routes/inventoryRoute.js"
+import cors from "cors"
+
 
 const app = express();
 
@@ -9,6 +11,7 @@ config({
 });
 
 // to check port is working or not start here
+app.use(cors())
 app.get("/", (req, res, next) => {
     res.status(200).send(`<h1>port working please check frontend:<a href=${process.env.FRONTEND_URL} > click here </a></h1>`);
 });
